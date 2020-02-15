67495e139ef18a4e3365700f7ed3fb92e1529db7
Raymond-601 <60190417+Raymond-601@users.noreply.github.com>
2020-02-15 14:34:20 -0500

    Update ui.r

--- app/ui.r
+++ app/ui.r
@@ -1,42 +1,45 @@
 library(shiny)
-library(leaflet)
+library(shinydashboard)
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("2009 Manhattan Housing Sales"),
-  
-  # Sidebar with a selector input for neighborhood
-  sidebarLayout(
-    sidebarPanel(
-      selectInput("nbhd", label = h5("Choose a Manhattan Neighborhood"), 
-                         choices = list("all neighborhoods"=0,
-                                        "Central Harlem"=1, 
-                                        "Chelsea and Clinton"=2,
-                                        "East Harlem"=3, 
-                                        "Gramercy Park and Murray Hill"=4,
-                                        "Greenwich Village and Soho"=5, 
-                                        "Lower Manhattan"=6,
-                                        "Lower East Side"=7, 
-                                        "Upper East Side"=8, 
-                                        "Upper West Side"=9,
-                                        "Inwood and Washington Heights"=10), 
-                         selected = 0)
-      #sliderInput("p.range", label=h3("Price Range (in thousands of dollars)"),
-      #            min = 0, max = 20000, value = c(200, 10000))
-    ),
-    # Show two panels
-    mainPanel(
-      #h4(textOutput("text")),
-      h3(code(textOutput("text1"))),
-      tabsetPanel(
-        # Panel 1 has three summary plots of sales. 
-        tabPanel("Sales summary", plotOutput("distPlot")), 
-        # Panel 2 has a map display of sales' distribution
-        tabPanel("Sales map", plotOutput("distPlot1"))),
-      leafletOutput("map", width = "80%", height = "400px")
+ui <-
+    dashboardPage(
+      skin = "red",
+      
+      dashboardHeader(title = "Rodent Inspection"),
+      
+      dashboardSidebar(
+        sidebarMenu(
+          menuItem("Menu1", tabName = "Menu1"),
+          menuItem("Menu2", tabName = "Menu2"),
+          menuItem("Menu3", tabName = "Menu3"),
+          menuItem("Menu4", tabName = "Menu4")
+        )
+      ),
+      
+      dashboardBody(
+        tabItems(
+          tabItem(tabName = "Menu1",
+                  fluidPage(
+                  )
+          ),
+          
+          tabItem(tabName = "Menu2",
+                  fluidPage(
+                  )
+          ),
+          
+          tabItem(tabName = "Menu3",
+                  fluidPage(
+                  )
+          ),
+          
+          tabItem(tabName = "Menu4",
+                  fluidPage(
+                  )
+          )
+        )
+      )
     )
- )
-))
+
+
 
