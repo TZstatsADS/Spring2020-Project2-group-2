005187e40b6228c0702522be7d53b25d05790929
Raymond-601 <60190417+Raymond-601@users.noreply.github.com>
2020-02-17 17:57:35 -0500

    Update Data_Cleaning_Process.R

--- doc/Data_Cleaning_Process.R
+++ doc/Data_Cleaning_Process.R
@@ -1,9 +1,9 @@
 library(tidyverse)
 
-arrest.clean %>% 
+aaa %>% 
   group_by(OFNS_DESC) %>% 
   summarize(count = n()) %>% 
-  arrange(desc(count)) -> a
+  arrange(desc(count))-> a
 
 
 arrest <- read_csv("../data/NYPD_Arrests_Data__Historic_.csv")
@@ -30,7 +30,6 @@ arrest.cleaned <-
                                  "Misdemeanor" = "M",
                                  "Violation" = "V",
                                  "Infraction" = "I")) %>% 
-  mutate(LAW_CAT_CD = as.character(LAW_CAT_CD)) %>% 
   mutate(JURISDICTION_CODE = factor(JURISDICTION_CODE,
                                     levels = c(0,1,2),
                                     labels = c("Patrol", "Transit", "Housing"))) %>% 
@@ -38,10 +37,62 @@ arrest.cleaned <-
   replace_na(list(JURISDICTION_CODE = "Others")) %>% 
   mutate(PERP_SEX = fct_recode(PERP_SEX,
                                "Male" = "M",
-                               "Female" = "F"))
+                               "Female" = "F")) %>% 
+  mutate(OFNS_DESC = factor(tolower(OFNS_DESC))) %>% 
+  mutate(OFNS_DESC = fct_collapse(OFNS_DESC,
+                                  "administrative code" = c("administrative code", "administrative codes"),
+                                  "burglary" = c("burglar's tools", "burglary"),
+                                  "child abandonment/non support" = c("child abandonment/non support", "child abandonment/non support 1"),
+                                  "criminal mischief & related offenses" = c("criminal mischief & related offenses", "criminal mischief & related of"),
+                                  "harassment" = c("harassment", "harrassment 2"),
+                                  "homicide-negligent" = c("homicide-negligent-vehicle", "homicide-negligent,unclassified"),
+                                  "intoxicated & impaired driving" = c("intoxicated & impaired driving", "intoxicated/impaired driving"),
+                                  "murder & non-negl. manslaughter" = c("murder & non-negl. manslaughter", "murder & non-negl. manslaughte"),
+                                  "off. agnst pub ord sensblty &" = c("off. agnst pub ord sensblty &", "off. agnst pub ord sensblty & rghts to priv"),
+                                  "offenses against public administration" = c("offenses against public administration", "offenses against public admini"),
+                                  "other state laws (non penal law)" = c("other state laws (non penal law)", "other state laws (non penal la"),
+                                  "possession of stolen property" = c("possession of stolen property", "possession of stolen property 5"))) %>% 
+  group_by(OFNS_DESC) %>% 
+  mutate(count = n()) %>% 
+  ungroup() %>% 
+  filter(count > 500) %>% 
+  mutate(OFNS_DESC = factor(as.character(OFNS_DESC))) %>% 
+  select(-c(count)) %>% 
+  mutate(OFNS_DESC = fct_collapse(OFNS_DESC,
+                                  "assault" = c("assault 3 & related offenses", 
+                                                "felony assault"),
+                                  "crime related to children" = c("child abandonment/non support",
+                                                                  "offenses related to children"),
+                                  "drug dealing" = c("dangerous drugs",
+                                                     "loitering for drug purposes"),
+                                  "fraud" = c("frauds",
+                                              "fraudulent accosting",
+                                              "theft-fraud",
+                                              "gambling"),
+                                  "gambling" = c("gambling",
+                                                 "loitering/gambling (cards, dice, etc)"),
+                                  "grand larceny" = c("grand larceny",
+                                                      "grand larceny of motor vehicle"),
+                                  "harassment" = c("forcible touching",
+                                                   "harassment"),
+                                  "kidnapping" = c("kidnapping & related offenses"),
+                                  "criminal mischief" = c("criminal mischief & related offenses"),
+                                  "murder" = c("murder & non-negl. manslaughter"),
+                                  "offenses against public safety" = c("off. agnst pub ord sensblty &",
+                                                                       "offenses against public administration",
+                                                                       "offenses against public safety",
+                                                                       "offenses against the person"),
+                                  "offenses state_law" = c("other state laws",
+                                                           "other state laws (non penal law)"),
+                                  "petit" = c("petit larceny",
+                                              "possession of stolen property"),
+                                  "prostitution" = c("prostitution & related offenses"),
+                                  "theft" = c("other offenses related to theft"),
+                                  "violate traffic laws" = c("intoxicated & impaired driving",
+                                                             "other traffic infraction",
+                                                             "unauthorized use of a vehicle 3 (uuv)",
+                                                             "vehicle and traffic laws")))
+  
 
-save()  
 
-
-
-
+save(arrest.cleaned, file = "/Users/ruozhou_zhang/Documents/statistic_S02/GR5243_Applied_Data_Science/Spring2020-Project2-group-2/output/arrest_cleaned.RData")
