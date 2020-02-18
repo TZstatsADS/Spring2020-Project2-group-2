20033059c7a8b0167ab51fbc59de848bc1c9771b
Raymond-601 <60190417+Raymond-601@users.noreply.github.com>
2020-02-18 15:04:28 -0500

    Create global.R

---
+++ app.page1.Ruozhou/global.R
@@ -0,0 +1,11 @@
+library(shiny)
+library(leaflet)
+library(leaflet.extras)
+library(googleVis)
+library(shinydashboard)
+#library(tidyverse)
+library(dplyr)
+library(shinyWidgets)
+
+
+load('/Users/ruozhou_zhang/Documents/statistic_S02/GR5243_Applied_Data_Science/Spring2020-Project2-group-2/output/arrest_cleaned.RData')
