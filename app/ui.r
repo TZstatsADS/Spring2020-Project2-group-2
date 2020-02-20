09211ea7c83d31cb63ce1530b24a61143cfcb4e8
Raymond-601 <60190417+Raymond-601@users.noreply.github.com>
2020-02-19 23:27:20 -0500

    Update ui.r

--- app/ui.r
+++ app/ui.r
@@ -1,17 +1,17 @@
 library(shiny)
-library(shinydashboard)
+
 
 ui <-
     dashboardPage(
       skin = "red",
       
-      dashboardHeader(title = "Rodent Inspection"),
+      dashboardHeader(title = "NYC Arrest Data"),
       
       dashboardSidebar(
         sidebarMenu(
-          menuItem("Map", tabName = "Map"),
-          menuItem("Menu2", tabName = "Menu2"),
-          menuItem("Menu3", tabName = "Menu3"),
+          menuItem("Map", tabName = "Map", icon = icon("map")),
+          menuItem("TimeSeries", tabName = "TimeSeries", icon = icon("chart-line")),
+          menuItem("Menu3", tabName = "Menu3", icon = icon("chart-pie")),
           menuItem("Menu4", tabName = "Menu4")
         )
       ),
@@ -19,13 +19,62 @@ ui <-
       dashboardBody(
         tabItems(
           tabItem(tabName = "Map",
-                  fluidPage(
-                    
-                  )
+                  fluidRow(
+                    box(
+                      width = 3,
+                      height = 80,
+                      dateInput("Map.starttime",
+                                "Start Time",
+                                value = "2018-01-01",
+                                min = min(arrest.cleaned$ARREST_DATE),
+                                max = max(arrest.cleaned$ARREST_DATE))
+                    ),
+                    box(
+                      width = 3,
+                      height = 80,
+                      dateInput("Map.endtime",
+                                "End Time",
+                                value = "2018-01-31",
+                                min = min(arrest.cleaned$ARREST_DATE),
+                                max = max(arrest.cleaned$ARREST_DATE))
+                    ),
+                    box(
+                      width = 6,
+                      height = 80,
+                      selectInput("Map.crimetype",
+                                  "Type of Crime",
+                                  choices = sort(unique(arrest.cleaned$OFNS_DESC)),
+                                  selected = "drug dealing",
+                                  multiple = T)
+                    )
+                  ),# select boxes end here
+                  fluidRow(
+                    box(
+                      width = 12,
+                      height = 700,
+                      ## The output is Here!
+                      leafletOutput("map.zrz", height = 680)
+                    )
+                  ) # map box
           ),
           
-          tabItem(tabName = "Menu2",
+          tabItem(tabName = "TimeSeries",
                   fluidPage(
+                    fluidRow(
+                      column(6,
+                             sliderInput(inputId = "year",label = "Choose a year",value = 2012,step=1,min = 2006,max = 2018)),
+                      
+                      column(6,
+                             selectInput(inputId = "choice_type",label ="choose a type",
+                                         choices =c(unique(as.character(arrest$OFNS_DESC)),"ALL"))),
+                      
+                      column(6,
+                             selectInput(inputId = "choice_borough",label = "choose a borough",choices =c(unique(arrest$ARREST_BORO),"ALL")))
+                    ),
+                    
+                    fluidRow(
+                      plotOutput(outputId = "ggplot",height = "600px")
+                    )
                   )
           ),
           
