f0c8f4a6532a27379c8bcf8fbf7331883902f24d
CarolinaBear <48881310+CarolinaBear@users.noreply.github.com>
2020-02-20 00:52:27 -0500

    Update global.R

--- app.heatmap.njy/global.R
+++ app.heatmap.njy/global.R
@@ -3,11 +3,21 @@ library(leaflet)
 library(googleVis)
 library(shinydashboard)
 library(leaflet.extras)
-#library(tidyverse)
+library(tidyverse)
 library(dplyr)
 library(shinyWidgets)
+library(shinyanimate)
+library(lubridate)
 
 load('../output/arrest_cleaned.RData')
 
-dt.ar <- as.data.frame(arrest.cleaned)
+arrest.cleaned.njy <- arrest.cleaned %>%
+  mutate(year = year(as.Date(ARREST_DATE,origin = "1970-01-01")),
+         month = month(as.Date(ARREST_DATE,origin = "1970-01-01")))
+
+arrest.cleaned.njy <- arrest.cleaned.njy %>%
+  mutate(qut = ifelse(month <=3, "Q1",ifelse(month <= 6, "Q2",ifelse(month <= 9,"Q3","Q4"))))
+
+arrest.cleaned.njy <- arrest.cleaned.njy %>%
+  mutate(yearq = paste(year,qut,sep = " "))
 
