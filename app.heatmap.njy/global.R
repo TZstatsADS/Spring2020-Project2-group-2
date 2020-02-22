fb63d21a3b39b1813abdbe5f170d8ebd6cc52f47
CarolinaBear <48881310+CarolinaBear@users.noreply.github.com>
2020-02-22 16:57:24 -0500

    update global.R for heatmap part

--- app.heatmap.njy/global.R
+++ app.heatmap.njy/global.R
@@ -11,13 +11,3 @@ library(lubridate)
 
 load('../output/arrest_cleaned.RData')
 
-arrest.cleaned.njy <- arrest.cleaned %>%
-  mutate(year = year(as.Date(ARREST_DATE,origin = "1970-01-01")),
-         month = month(as.Date(ARREST_DATE,origin = "1970-01-01")))
-
-arrest.cleaned.njy <- arrest.cleaned.njy %>%
-  mutate(qut = ifelse(month <=3, "Q1",ifelse(month <= 6, "Q2",ifelse(month <= 9,"Q3","Q4"))))
-
-arrest.cleaned.njy <- arrest.cleaned.njy %>%
-  mutate(yearq = paste(year,qut,sep = " "))
-
