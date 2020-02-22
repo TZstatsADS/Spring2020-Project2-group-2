206af7e83dd672052895f361c6c4ca49b811f422
SaierGong7 <60413562+SaierGong7@users.noreply.github.com>
2020-02-22 17:03:16 -0500

    Update server_saier.R

--- app.time.saier/server_saier.R
+++ app.time.saier/server_saier.R
@@ -1,23 +1,24 @@
+#### Time Series Graph
 server<-function(input,output){
-  output$ggplot<-renderPlot({
-    y<-input$year
-    type<-input$choice_type
-    borough<-input$choice_borough
-    
-    if(type=="ALL"&borough=="ALL"){
-      return(aa(data=arrest,y=y))
-    }else if(type=="ALL"&borough!="ALL"){
-      return(tt(data=arrest,borough=borough,y=y))
-    }else if(type!="ALL"&borough=="ALL"){
-      return(qq(data=arrest,type=type,y=y))
-    }else{
-      return(pp(data=arrest,type = type,borough=borough,y=y))
-    }
-    
-    
-    
-  })
+  df.timeseries<-reactive({
+    arrest.cleaned %>%
+      mutate(year=year(ARREST_DATE),
+           month=month(ARREST_DATE))
+})
+output$ggplot<-renderPlot({
+  y<-input$year
+  type<-input$choice_type
+  borough<-input$choice_borough
   
-  
-  
+  if(type=="ALL"&borough=="ALL"){
+    return(aa(data=df.timeseries(),y=y))
+  }else if(type=="ALL"&borough!="ALL"){
+    return(tt(data=df.timeseries(),borough=borough,y=y))
+  }else if(type!="ALL"&borough=="ALL"){
+    return(qq(data=df.timeseries(),type=type,y=y))
+  }else{
+    return(pp(data=df.timeseries(),type = type,borough=borough,y=y))
+  }
+})
+
 }
