350713e2b291388baa713172024670b3c1f6a783
Raymond-601 <60190417+Raymond-601@users.noreply.github.com>
2020-02-24 22:31:07 -0500

    Update ui.r

--- app/ui.r
+++ app/ui.r
@@ -1,18 +1,20 @@
-library(shiny)
-
-
 ui <-
     dashboardPage(
       skin = "red",
       
-      dashboardHeader(title = "NYC Arrest Data"),
+      dashboardHeader(title = "NYC Arrest Data",
+                      dropdownMenu(type = "messages",
+                                   messageItem(from = "Data Resource",
+                                               message = "NYC OPEN DATA",
+                                               icon = icon("database"))
+                                   )),
       
       dashboardSidebar(
         sidebarMenu(
-          menuItem("Map", tabName = "Map", icon = icon("map"),
+          menuItem("Map", tabName = "Map", icon = icon("map-marked-alt"),
                    menuSubItem("Crime Map", tabName = "Map"),
                    menuSubItem("Heat Map", tabName = "Animation")),
-          menuItem("TimeSeries", tabName = "TimeSeries", icon = icon("chart-line")),
+          menuItem("TimeSeries", tabName = "TimeSeries", icon = icon("shoe-prints")),
           menuItem("PieChart", tabName = "PieChart", icon = icon("chart-pie")),
           menuItem("More Info", tabName = "MoreInfo", icon = icon("info"))
         )
@@ -142,30 +144,34 @@ ui <-
                       box(
                         width = 12,
                         title = "The Problem (Motivation)",
-                        solidHeader = TRUE,
-                        h4("New York City is huge metropolis, attracting people from around the world, and an important issue for many living (or those wanting to live) in NYC is public safety.  Crime occurs all the time: from assaults to murders to grand larceny to drunk driving. Residents, visitors, police officials, and policy makers should be better informed of which areas are more likely to be dangerous, what types of crime are more likely to occur, and what times of the year crime is likely; however, it is not always easy for average people to find this information. It is our hope that this app will remedy this by making information on crime more accessible.")
+                        h5("  New York City is huge metropolis, attracting people from around the world, and an important issue for many living (or those wanting to live) in NYC is public safety.  Crime occurs all the time: from assaults to murders to grand larceny to drunk driving. Residents, visitors, police officials, and policy makers should be better informed of which areas are more likely to be dangerous, what types of crime are more likely to occur, and what times of the year crime is likely; however, it is not always easy for average people to find this information. It is our hope that this app will remedy this by making information on crime more accessible.")
                       )
                     ),
                     fluidRow(
                       box(
                         width = 12,
                         title = "The Solution",
-                        solidHeader = TRUE,
-                        h4("Our shiny app is based on the official NYPD arrest records in New York City of the last 5 years (2013-2018).  The target users for this project, include residents, the NYPD and Policy Makers. Through our app, residents can know where different kinds of criminals appear and are arrested during each year and each borough, so that they can be informed of when and where criminals are more likely to appear and keep away from such areas. For police, given the past data, they can make predictions as to when and where different types of criminals will appear and try their best to catch them and maintain public safety. Policy makers will be able to study demographics of the arrests (age, gender, and race) for different types of crimes, different years, and boroughs and determine whether unfair bias exists based on different boroughs such as racism and gender inequality.")
+                        h5("  Our shiny app is based on the official NYPD arrest records in New York City of the last 5 years (2013-2018).  The target users for this project, include residents, the NYPD and Policy Makers. Through our app, residents can know where different kinds of criminals appear and are arrested during each year and each borough, so that they can be informed of when and where criminals are more likely to appear and keep away from such areas. For police, given the past data, they can make predictions as to when and where different types of criminals will appear and try their best to catch them and maintain public safety. Policy makers will be able to study demographics of the arrests (age, gender, and race) for different types of crimes, different years, and boroughs and determine whether unfair bias exists based on different boroughs such as racism and gender inequality.")
                       )
                     ),
                     fluidRow(
                       box(
                         width = 12,
                         title = "The Details",
-                        solidHeader = TRUE,
-                        h3("Our NYC Arrest Data App features 3 primary functions:"),
+                        h4("Our NYC Arrest Data App features 3 primary functions:"),
                         tags$div(tags$ul(
                           tags$li("Map - Crime Map: Shows locations and specific details of each arrest (date, jurisdiction, crime type, and level of offense) within a specified range of time.  The users can also search for crimes by type."),
                           tags$li("Map - Heat map: Shows animated density of arrests made each year by crime type."),
                           tags$li("TimeSeries: Shows a time-series plot of the number of crimes over 12 months. The plot can change by specifying the year, crime type, and borough."),
                           tags$li("Pie Chart: Shows three different pie charts that detail the arrest demographics (sex, race, and age) by year, crime type, and borough. ")
                         ))
+                      )
+                    ),
+                    fluidRow(
+                      box(
+                        width = 12,
+                        title = "Developers",
+                        h5("Ruozhou Zhang, Saier Gong, Jiayun Ni, Lu Cheng, Tianning Yu, Heagy David")
                       )
                     )
                   )
